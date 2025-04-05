b2a1113850e08185b87a84dc848b938d4cfe52a6
Luigi de Souza <luigi.de.sou@gmail.com>
2025-04-05 00:34:45 -0300

    task: adding DELETE and GET from ID and updating the url

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, request, jsonify
+from flask import Flask, request, jsonify
 from bd import Itens
 
 app = Flask(__name__)
@@ -14,8 +14,38 @@ def getItens():
 def creatItens():
     iten = request.json
     Itens.append(iten)
-    return iten
-    # return make_response(jsonify(message="Roupa cadastrada com sucesso", iten=iten))
+    return jsonify(message="Roupa cadastrada com sucesso", iten=iten)
 
 
-app.run()
+@app.route("/itens/<id>", methods=['GET'])
+def showItem(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return jsonify({"error": "ID invalid"}), 400
+
+    item = next((item for item in Itens if item["id"] == int(id)), None)
+
+    if item:
+        return jsonify(item), 200
+    else:
+        return jsonify({"error": "Item not found"}), 404
+
+
+@app.route("/itens/<id>", methods=['DELETE'])
+def deleteItem(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return jsonify({"error": "ID invalid"}), 400
+    
+    item = next((item for item in Itens if item["id"] == id), None)
+
+    if item:
+        Itens.remove(item)
+        return Itens
+    else:
+        return jsonify({"error": "Item not found"}), 404
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True)
